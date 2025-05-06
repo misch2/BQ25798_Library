eac3a59f30ed176454246ab2784a233c814475d5
Michal Schwarz <michal.schwarz@giacom.com>
2025-05-06 15:06:44 +0200

    fix the "no i2c" variant

--- lib/BQ25798NoI2C/BQ25798NoI2C.h
+++ lib/BQ25798NoI2C/BQ25798NoI2C.h
@@ -7,6 +7,15 @@
 class BQ25798NoI2C : public BQ25798Core<I2CGeneric> {
  public:
   BQ25798NoI2C() {};
+
+  bool i2c_begin(uint8_t chip_address) override { return false; }
+  void i2c_end() override {};
+
+  bool i2c_isConnected() override { return false; }
+  uint8_t i2c_readReg8(int reg) override { return 0; };
+  void i2c_writeReg8(int reg, uint8_t value) override {};
+  uint16_t i2c_readReg16(int reg) override { return 0; };
+  void i2c_writeReg16(int reg, uint16_t value) {};
 };
 
 #endif
